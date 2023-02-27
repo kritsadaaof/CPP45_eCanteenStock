bf1ea5c85f36e8d860549250967184cc5e3a256e
kritsadaaof <56905496+kritsadaaof@users.noreply.github.com>
2023-02-27 10:49:52 +0700

    Stock
    Stock

---
+++ Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CPP45_eCanteenStock.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+    }
+}
