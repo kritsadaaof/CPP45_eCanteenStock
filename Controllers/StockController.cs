2eb32f807a04261307a1710eafe3f46343cd451e
kritsadaaof <56905496+kritsadaaof@users.noreply.github.com>
2023-02-28 11:47:03 +0700

    Connect DB

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CPP45_eCanteenStock.Models;
 
 namespace CPP45_eCanteenStock.Controllers
 {
+   
     public class StockController : Controller
     {
+       private CPP45_eCanteenEntities DbFile = new CPP45_eCanteenEntities();  //DB
         // GET: Stock
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Products()
-        {
+        { 
             return View();
         }
 
