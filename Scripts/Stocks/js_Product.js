74c85f83f291201a06c3e2dab791c2bd5dbc6500
dechathon dilokmontree <dechathon.di@gmail.com>
2023-03-01 10:53:20 +0700

    Create js_Product.js

---
+++ Scripts/Stocks/js_Product.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(() => {
+    $("#Summit_Create").click(() => {
+        var inputProduct = $("#inputId, #inputName, #inputBuy, #inputQuantity, #inputPrice, #inputData").val();
+        if (inputProduct.trim() === "") { // Check if the input value is null or empty
+            alert("กรุณากรอกข้อมูล ให้ครบ"); // Show an alert asking the user to enter data
+            $("#inputId, #inputName, #inputBuy, #inputQuantity, #inputPrice, #inputData").addClass("is-invalid"); // Add "is-invalid" class to show a request tag
+            return;
+        }
+        $.post(baseUrl + "Stock/Summit", {
+            ADDID: $("#inputId").val(),
+            ADDNAME: $("#inputName").val(),
+            ADDBUY: $("#inputBuy").val(),
+            ADDQTY: $("#inputQuantity").val(),
+            ADDPRICE: $("#inputPrice").val(),
+            ADDDATE: $("#inputData").val()
+        }).done((data) => {
+            alert("บันทึกชื่อของคุณสำเร็จแล้ว");
+            location.reload();
+        });
+    });
+});
